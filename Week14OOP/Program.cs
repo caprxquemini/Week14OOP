a41ce6925dffab85b88af13a1c92dd6187b0fe83
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-23 16:28:25 +0300

    Add project files.

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+Dog goodboy = new Dog("Good Boy");
+
+Console.WriteLine($"Name: {goodboy.Name}; Level of happiness: {goodboy.LevelOfHappiness}");
+
+while (goodboy.LevelOfHappiness < 5)
+{
+    goodboy.Bark();
+}
+
+goodboy.Wag();
+Console.WriteLine($"Level of happiness: {goodboy.LevelOfHappiness}");
+
+class Dog
+    {
+    public string Name;
+    public int LevelOfHappiness;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappiness = 0;
+        Console.WriteLine($"{Name} is your friend now!");
+    }
+
+
+    public void Bark()
+    {
+        LevelOfHappiness++;
+        Console.WriteLine("Woof woof!");
+       
+    }
+
+    public void Wag()
+    {
+        Console.WriteLine($"{Name} starts wagging its tail.");
+        Console.WriteLine("Whoosh-whoosh!");
+    }
+}
+
